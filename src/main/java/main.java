bd41ede324db24e357e7d77d5c078102c74dcfff
rjkw <89926@farnborough.ac.uk>
2019-09-20 12:07:18 +0100

    Database connection established and disconnected nice.

--- src/main/java/main.java
+++ src/main/java/main.java
@@ -1,2 +1,48 @@
+import org.sqlite.SQLiteConfig;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+
+
 public class main {
+    public static Connection db = null;
+    public static void main(String[] args) {
+
+        openDatabase("Words.db");
+        closeDatabase();
+    }
+    private static void openDatabase(String dbFile)
+    {
+
+        try  {
+
+            Class.forName("org.sqlite.JDBC");
+            SQLiteConfig config = new SQLiteConfig();
+            config.enforceForeignKeys(true);
+            db = DriverManager.getConnection("jdbc:sqlite:resources/" + dbFile, config.toProperties());
+            System.out.println("Database connection successfully established.");
+
+        } catch (Exception exception) {
+
+            System.out.println("Database connection error: " + exception.getMessage());
+        }
+
+    }
+
+    private static void closeDatabase()
+    {
+        try {
+
+            db.close();
+            System.out.println("Disconnected from database.");
+
+        } catch (Exception exception) {
+
+            System.out.println("Database disconnection error: " + exception.getMessage());
+        }
+    }
+
+
+
 }
+
