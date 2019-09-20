f12d3b6ed54670a26ecb9ad864d9673e01777733
rjkw <89926@farnborough.ac.uk>
2019-09-20 12:13:58 +0100

    We did read crud i think

--- src/main/java/leaderboardController.java
+++ src/main/java/leaderboardController.java
@@ -1,4 +1,27 @@
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+
 public class leaderboardController {
+    public static void listThings() {
+
+        try {
+
+            PreparedStatement ps = main.db.prepareStatement("SELECT Id, Name, Quantity FROM Things");
+
+            ResultSet results = ps.executeQuery();
+            while (results.next()) {
+                int Score = results.getInt(1);
+                int Placement = results.getInt(2);
+                String userName = results.getString(3);
+                System.out.print("Id: " + Score + ",  ");
+                System.out.print("Name: " + Placement + ",  ");
+                System.out.print("Quantity: " + userName + "\n");
+            }
+
+        } catch (Exception exception) {
+            System.out.println("Database error: " + exception.getMessage());
+        }
+    }
 
 
 }
