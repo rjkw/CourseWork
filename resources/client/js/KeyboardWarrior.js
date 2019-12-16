7cbcaa5dc7a62c2be842629d435ba2470b6fc920
rjkw <89926@farnborough.ac.uk>
2019-12-16 16:11:29 +0000

    We have a menu. It doesnt do much, but its a menu

--- resources/client/js/KeyboardWarrior.js
+++ resources/client/js/KeyboardWarrior.js
@@ -13,8 +13,9 @@ function pageLoad() {
 
     window.addEventListener("resize", fixSize);
     fixSize();
-
     window.requestAnimationFrame(redraw);
+
+    preparewords();
 
 
 
@@ -24,11 +25,11 @@ function redraw() {
     const canvas = document.getElementById('typingcanvas');
     const context = canvas.getContext('2d');
 
-    context.fillStyle = 'gray';
-    context.beginPath();
-    context.rect(0, 0, w,h);
-    context.fill();
-
+    /*  context.fillStyle = 'gray';
+     context.beginPath();
+     context.rect(0, 0, w,h);
+     context.fill();
+ */
     window.requestAnimationFrame(redraw)
 }
 
@@ -44,36 +45,57 @@ class Words {
         this.edge = false;
         this.alive = true;
 
-function prepareWords() {  // Starting position of word
-    let y = h/2;
-    let x = 100;
-}
+
+    }
+
+    draw(context) {
+
+        context.strokeStyle = 'black';
+        context.strokeText("Test");
+
     }
 
     update(frameLength) {
 
         this.edge = false;
         this.x += frameLength * this.dx;
-        if (this.x > w - 50) this.edge = true;
-        this.alive=false;
-        console.log("You dead");
+        if (this.x < 50 || this.x > w - 50) this.edge = true;
+
+        this.reload -= frameLength;
+
+    }
+}
+
+function preparewords() {
+    let x = 0;
+    let y = h / 2;
+
+    Words.draw = function (context) {
+        context.strokeStyle = 'black';
+        context.strokeText("Test");
+
     }
 
-    function gameFrame(timestamp) {
+
+    function gameFrame(timestamp) { //
 
         if (lastTimestamp === 0) lastTimestamp = timestamp;
         const frameLength = (timestamp - lastTimestamp) / 1000;
         lastTimestamp = timestamp;
 
         // inputs();
-       // processes(frameLength);
-        // outputs();
+        // processes(frameLength);
+        outputs();
 
         window.requestAnimationFrame(gameFrame);
 
     }
 
+    function outputs() {
+        const canvas = document.getElementById('invadersCanvas');
+        const context = canvas.getContext('2d');
+
+        Words.draw();
+
+    }
 }
-
-
-
